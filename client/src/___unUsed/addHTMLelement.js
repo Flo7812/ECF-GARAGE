47be80e0d24ecb88b325a5605aba6a6f1a379006
Florent Malatrat <ft.malatrat@hotmail.fr>
2024-02-24 14:27:36 +0100

    gitignore

--- client/src/___unUsed/addHTMLelement.js
+++
@@ -1,9 +0,0 @@
-
-export default function addHTML(tagName, text = '', atributs = {} ){
-    const element = document.createElement(tagName)
-    element.textContent = text
-    for(const [atr, value =''] of Object.entries(atributs)){
-        element.setAttribute(atr, value)
-    }
-    return element
-}
